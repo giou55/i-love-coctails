52887a3cbfd991a32d649a52883ecb4c29ba3a30
Giorgos Giourmetakis <giorgosggg@gmail.com>
2020-02-10 02:37:22 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ILoveCoctails';
+
+  constructor(private http: HttpClient) {}
+
+  coctail:any = {
+    name:"",
+    imgPath:""
+  }; 
+
+  onFetchCoctail1() {
+    this.http
+      .get<any>(
+        'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita'
+      )
+      .subscribe(
+        responseData => {
+          console.log(responseData);
+          this.coctail.name = responseData.drinks[1].strDrink; 
+          this.coctail.imgPath = responseData.drinks[1].strDrinkThumb;
+        }
+      );
+  }
+
+  onFetchCoctail2() {
+    this.http
+      .get<any>(
+        'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita'
+      )
+      .subscribe(
+        responseData => {
+          console.log(responseData);
+        }
+      );
+  }
+
+
 }
